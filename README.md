90faf9c199edb8fa90844b4b6716ce3138336fc7
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-09-18 22:54:18 -0400

    adding to logic file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-## P2 README file
+README.md
+
+# Pauline Shoemaker's p2
+
+## Live URL
+<http://p2.paulineshoemaker.me/>
+
+## Description- PHP Basics
+The aim is to create a password generator alike to the xkcd examples given. This should include forms, arrays, and server side validation/
+
+### Needs to show:
+1. How many words to use
+2. If the user would like to include a number
+3. If the use would like to include a symbol
+4. Basic server side validation is required, client side validation is encouraged
+
+## Demo
+<Insert here>
+
+## Outside materials
++ xkcd examples
--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<h1> Getting p2 to work</h1>
+<?php
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>p2 password generator</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="" type="text/css">
+
+    <?php require ('logic.php'); ?>
+
+	<body>
+    <h1>p2: Hungry for a password?</h1>
+
+  <form action='index.php' method='GET'>
+  </form>
+    <input type='text' name='numWords'><br>
+    <input type='radio' name='yesOrNo' value='yes'>Yes<br>
+    <input type='radio' name='yesOrNo' value='no'>No<br>
+    <input type='submit' value='Get a password!'>
+  </form>
+
+	</body>
+</html>
