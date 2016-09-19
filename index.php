a630d4ddcee4959a7cb193b6a6b0df82cf01e42f
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-09-19 17:39:02 -0400

    finally all is working

--- index.php
+++ index.php
@@ -3,17 +3,22 @@ error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
 ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
 
+<?php
+phpinfo();
+?>
+
+
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>p2 password generator</title>
+		<title>p2 pw generator</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="" type="text/css">
 
     <?php require ('logic.php'); ?>
 
 	<body>
-    <h1>p2: Hungry for a password?</h1>
+    <h1>p2: Hungry for a password? CHANGE AGAIN</h1>
 
   <form action='index.php' method='GET'>
   </form>
