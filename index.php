f22cb5b3e484c24e9675c194df8d7c85b9778714
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-09-20 10:58:45 -0400

    updating css/html

--- index.php
+++ index.php
@@ -1,32 +1,51 @@
+<!-- Error checker -->
 <?php
 error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
 ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
 
-<?php
-phpinfo();
-?>
 
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>p2 pw generator</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="" type="text/css">
+		<title>p2</title>
+		<meta charset="utf-8">
 
+		<!-- Add style sheets -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/initialize.css" type="text/css">
+		<link rel="stylesheet" href="css/p2css.css" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+
+		<!-- attach to logic file: SOC -->
     <?php require ('logic.php'); ?>
 
-	<body>
-    <h1>p2: Hungry for a password? CHANGE AGAIN</h1>
+	</head>
 
-  <form action='index.php' method='GET'>
-  </form>
-    <input type='text' name='numWords'>Enter desired number of words<br>
-    <input type='radio' name='yesOrNo' value='yes'>Yes<br>
-    <input type='radio' name='yesOrNo' value='no'>No<br>
-    <input type='submit' value='Get a password!'>
-  </form>
+	<body>
+		<img class="snacks" src="http://www.clipartkid.com/images/249/healthy-food-border-clipart-panda-free-clipart-images-jUOQXH-clipart.jpg" alt="snacks"></a>
+
+    <h1>Hungry for a password?..</h1>
+
+		<form class="form" action='index.php' method='GET'>
+
+			<p class='pwform'>
+				<label for="howManyWords">How many words long? Max of 5</label>
+				<br>
+				<input type='number' name='howManyWords' min='1' max='5' id='howManyWords'>
+				<br>
+
+				<input type='checkbox' name='add_number' id='add_number' >
+				<label for='add_number'>Add a number?</label>
+				<br>
+				<input type='checkbox' name='add_symbol' id='add_symbol' >
+				<label for='add_symbol'>A symbol too?!</label>
+			</p>
+
+			<input type='submit' class='button' name="button" value='Snack on!'>
+
+		</form>
 
 	</body>
 </html>
--- logic.php
+++ logic.php
@@ -1,1 +1,60 @@
 <?php
+// $foodFile = fopen('foodwords.csv', 'r');
+//   while (($foodArray = fgetcsv($foodFile)) !== FALSE) {
+//     print_r($foodArray);
+//   }
+//   fclose($foodFile);
+//
+//   count($foodArray)
+
+
+// $file = file_get_contents("foodwords.csv");
+// $data = array_map("str_getcsv", preg_split('/\r*\n+|\r+/', $file));
+// print_r($data);
+//
+// count(foodArray)
+//
+// $foodWords = fgetcsv('foodwords.csv');
+// count($foodWords)
+//
+// $_POST["numberofwords"] will give you access to the value of input element `<input type="number" name="numberofwords">`
+
+// $foodWords = fgetcsv("foodwords.csv","r");
+
+// $foodWords = array_map('str_getcsv', file('foodwords.csv'));
+
+
+
+$file = fopen('foodwords.csv', 'r');
+while (($foodWords = fgetcsv($file)) !== FALSE) {
+  echo "string"; '</pre>';
+  echo $foodWords;
+  echo '</pre>';
+}
+fclose($file);
+
+// Counts number of elements in an array
+echo count($foodWords);
+
+
+
+
+// $foodWords = fopen('foodwords.csv', 'r');
+// echo '<pre>';
+// print_r(fgetcsv($foodWords));
+// fclose($foodWords);
+// echo var_dump($foodWords);
+// echo '</pre>';
+// echo count($foodWords);
+// ?>
+
+<!-- Get array from CSV -->
+<!-- $foodArray = [...]; -->
+<!-- array = -->
+<!-- $foodArray = [...]; -->
+<!-- shuffle the array -->
+<!-- shuffle($foodArray); -->
+<!-- for each, takes the last element of an array -->
+<!-- while($element = array_pop($foodArray)){ -->
+  <!-- echo 'pwElement:' . $element; -->
+<!-- } -->
