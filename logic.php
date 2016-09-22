ef1367f422f383edb699eb40ab6fbe322e0781a4
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-09-22 11:14:04 -0400

    try commited final code again

--- logic.php
+++ logic.php
@@ -1,11 +1,10 @@
 <?php
 
-# Make character string into an array
+# Make hard-code character string into an array
 $SYMS = str_split("@#$%^&*=+?");
-# Make number string into an array
+# Make hard-code number string into an array
 $NUMS = str_split("1234567890");
 $password = "";
-// $desiredLength = "";
 
 # IMPORT CSV FOOD WORD ARRAY---------------------------
 $file = fopen("foodwords.csv","r");
@@ -13,18 +12,23 @@ $foodList = explode(chr(13), fgets($file));
 fclose($file);
 
 # FORM DATA ------------------------------
-# Will deplay this code only if there is information entered into form with isset
 if (isset($_GET["howManyWords"])){
-# Need the number of words to be manipulated as a number, also easier to write "numWords" than _GET[howManyWords]
+
+#Use as integer
 $numWords = intval($_GET["howManyWords"]);
+
+    # Some simple form validation
+    if(!ctype_alnum($numWords)) {
+    $error = 'Contestant names may only contain letters; no numbers or symbols.';
+    		return;
+    	}
+
 # If/else statement to return random keys from foodList array
 if ($numWords !== 0){
 $rand_words = array_rand($foodList,$numWords);
 }
 
 # PRINT PASSWORD -------------------------
-# Create password variable to print the generated password
-# For loop to print password based on given numWords length from form
 for($i = 0; $i < $numWords; $i++){
     if ($i == 0){
         $password = $password . $foodList[$rand_words[$i]];
@@ -33,7 +37,7 @@ for($i = 0; $i < $numWords; $i++){
         $password = $password . " - " . $foodList[$rand_words[$i]];
     }
 }
-# Continue to add number and unique character to password variable
+# Continue to add number and unique characters to password variable
 if($numWords !== 0){
     if( array_key_exists('add_number',$_GET)){
             $NUMS = str_split("1234567890");
