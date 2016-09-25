995414bd91cb9fbaecc2c6214a2d0bb6525f490f
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-09-25 18:02:49 -0400

    correcting code indentation

--- index.php
+++ index.php
@@ -6,45 +6,41 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>p2</title>
-		<meta charset="utf-8">
+<head>
+	<title>p2</title>
+	<meta charset="utf-8">
 
-		<!-- Add style sheets -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link rel="stylesheet" href="css/initialize.css" type="text/css">
-		<link rel="stylesheet" href="css/p2css.css" type="text/css">
-		<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Pavanam" rel="stylesheet">
-		<!-- attach to logic file: SOC -->
-    <?php require ('logic.php'); ?>
+	<!-- Add style sheets -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/initialize.css" type="text/css">
+	<link rel="stylesheet" href="css/p2css.css" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Pavanam" rel="stylesheet">
+	<!-- attach to logic file: SOC -->
+	<?php require ('logic.php'); ?>
 
-	</head>
+</head>
 
-	<body>
-		<img class="snacks" src="http://www.clipartkid.com/images/249/healthy-food-border-clipart-panda-free-clipart-images-jUOQXH-clipart.jpg" alt="snacks">
+<body>
+	<img class="snacks" src="http://www.clipartkid.com/images/249/healthy-food-border-clipart-panda-free-clipart-images-jUOQXH-clipart.jpg" alt="snacks">
+	<h1>Hungry for a password?..</h1>
+	<h2><?php if (isset($numWords) && ($numWords == 0)){ echo "Please enter how many words you'd like.";} ?></h2>
+	<h2><?php echo $password ?></h2>
+	
+	<form class="form" action='index.php' method='GET'>
+		<p class='pwform'>
+			<label for="howManyWords">How many words long? Max of 5</label>
+			<br>
+			<input type='number' name='howManyWords' min='1' max='5' id='howManyWords'>
+			<br>
+			<input type='checkbox' name='add_number' id='add_number' >
+			<label for='add_number'>Add a number?</label>
+			<br>
+			<input type='checkbox' name='add_symbol' id='add_symbol' >
+			<label for='add_symbol'>A symbol too?!</label>
+		</p>
+		<input type='submit' class='button' name="button" value='Snack on!'>
+	</form>
 
-    <h1>Hungry for a password?..</h1>
-		<h2><?php if (isset($numWords) && ($numWords == 0)){ echo "Please enter how many words you'd like.";} ?></h2>
-		<h2><?php echo $password ?></h2>
-		<form class="form" action='index.php' method='GET'>
-
-			<p class='pwform'>
-				<label for="howManyWords">How many words long? Max of 5</label>
-				<br>
-				<input type='number' name='howManyWords' min='1' max='5' id='howManyWords'>
-				<br>
-
-				<input type='checkbox' name='add_number' id='add_number' >
-				<label for='add_number'>Add a number?</label>
-				<br>
-				<input type='checkbox' name='add_symbol' id='add_symbol' >
-				<label for='add_symbol'>A symbol too?!</label>
-			</p>
-
-			<input type='submit' class='button' name="button" value='Snack on!'>
-
-		</form>
-
-	</body>
+</body>
 </html>
--- logic.php
+++ logic.php
@@ -14,42 +14,43 @@ fclose($file);
 # FORM DATA ------------------------------
 if (isset($_GET["howManyWords"])){
 
-#Use as integer
-$numWords = intval($_GET["howManyWords"]);
+    #Use as integer
+    $numWords = intval($_GET["howManyWords"]);
 
     # Some simple form validation
     if(!ctype_alnum($_GET["howManyWords"])) {
-    $error = 'Contestant names may only contain letters; no numbers or symbols.';
-    		return;
-    	}
-
-# If/else statement to return random keys from foodList array
-if ($numWords !== 0){
-$rand_words = array_rand($foodList,$numWords);
-}
-
-# PRINT PASSWORD -------------------------
-for($i = 0; $i < $numWords; $i++){
-    if ($i == 0){
-        $password = $password . $foodList[$rand_words[$i]];
+        $error = 'Contestant names may only contain letters; no numbers or symbols.';
+        return;
     }
-    else {
-        $password = $password . " - " . $foodList[$rand_words[$i]];
+
+    # If/else statement to return random keys from foodList array
+    if ($numWords !== 0){
+        $rand_words = array_rand($foodList,$numWords);
     }
-}
-# Continue to add number and unique characters to password variable
-if($numWords !== 0){
-    if( array_key_exists('add_number',$_GET)){
+
+    # PRINT PASSWORD -------------------------
+    for($i = 0; $i < $numWords; $i++){
+        if ($i == 0){
+            $password = $password . $foodList[$rand_words[$i]];
+        }
+        else {
+            $password = $password . " - " . $foodList[$rand_words[$i]];
+        }
+    }
+    # Continue to add number and unique characters to password variable
+    if($numWords !== 0){
+        if( array_key_exists('add_number',$_GET)){
             $NUMS = str_split("1234567890");
             shuffle($NUMS);
             $passwordNum= end($NUMS);
             $password .= " - " . $passwordNum;
         }
-    if( array_key_exists('add_symbol',$_GET)){
+        if( array_key_exists('add_symbol',$_GET)){
             $SYMS = str_split("@#$%^&*=+?");
             shuffle($SYMS);
             $passwordSym = end($SYMS);
             $password .= " - " . $passwordSym;
+        }
+        # echo $password; to test
     }
-# echo $password; to test
-}}
+}
